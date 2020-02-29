e871919814fae5aa535871ae5598830b2678a84c
sinbrive <21102151+sinbrive@users.noreply.github.com>
2020-02-29 15:06:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -63,33 +63,4 @@
 */
 		?>			
 		
-
-<?php 
-/*
-		$username = "sinbrive"; 
-		$pass = "xxxx"; 
-		$host = "db4free.net:3306"; 
-		$dbname = "sinbase"; 
-		
-		$con = mysqli_connect($host, $username, $pass);
-		if (!$con) {
-		  die('Could not connect: ' . mysqli_error($con));
-		}
-
-		mysqli_select_db($con, $dbname) or die ('Can\'t use foo : ' . mysqli_error($con));
-		
-		$query = "SELECT * FROM table_mesures";
-
-		$result = mysqli_query( $con, $query ); 
-		if (!$result) {
-			echo "Erreur : " . mysqli_error($con);
-		} else {
-			while ($record = mysqli_fetch_row($result)) {
-				echo "$record[1]  $record[0] \n";
-			}
-		}
-		mysqli_close($con);
-*/
-		?>	
-
 		
