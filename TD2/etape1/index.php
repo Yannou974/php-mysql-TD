c8c4e39d4a6e64344443558e0a25bf6fa4d17708
sinbrive <21102151+sinbrive@users.noreply.github.com>
2020-05-17 10:32:12 +0200

    Create index.php

---
+++ TD2/etape1/index.php
@@ -0,0 +1,31 @@
+<!-- Etape 1 : tester le formulaire avec php -->
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>TD2-SIN 2020</title>
+		<style type="text/css"></style>
+	</head>
+	<body>
+        <form action="" method ="POST">
+            <input type="text" name="nom" value="">
+            ______________________________________
+            ______________________________________
+            ______________________________________
+            ______________________________________
+        </form>
+        <?php
+            // récupération des données du formulaire
+            $nom= $_POST['nom'];	
+            $prenom= _____________
+            $statut= _____________
+            $date= _____________
+            //afficher les données ici dans des div 
+            echo "<div>_____________<div>";
+            _____________
+            _____________
+            _____________
+        ?>
+    </body>
+  
+</html>
+
